35c1e272b8fba92dd88e770bee3cfa582750801f
Andrew Simmons <neon.duncan@gmail.com>
2020-12-03 21:30:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ function shout(string){
 }
 
 function whisper(string){
-  return string.toLowerCase
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if 
+  
 }
